f4c7bebce2f67fad4403b16ef3dffa31032673e6
KoshiKajimura <absolute_zero_degree000@yahoo.co.jp>
2020-08-14 16:34:12 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "rooms#index"
 
-  resources :users, only: [:edit, :update]
-  resources :rooms, only: [:index, :new, :create]
+  resources :users, only: [:edit, :update] 
+  resources :rooms, only: [:index, :new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
